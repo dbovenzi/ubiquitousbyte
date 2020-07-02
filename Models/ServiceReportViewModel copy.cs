1d7aeea80b5a2dcf06b79e8348e1bd58487253ab
Daniel Bovenzi <danielbovenzi@macbook-pro.home>
2020-07-02 18:05:33 +0100

    added service report

---
+++ Models/ServiceReportViewModel copy.cs
@@ -0,0 +1,13 @@
+
+using System;
+
+namespace ubiquitousbyte.Models
+{
+    public class ServiceReport
+    {
+        public string ServiceName { get; set; }
+        public int ServiceStatus {get;set;}
+        public string ServiceStatusMessage {get;set;}
+
+    }
+}
